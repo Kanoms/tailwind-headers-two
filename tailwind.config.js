965412c1ebc8a104a58532593f08c50013042d97
Kelechi <kanomneze@gmail.com>
2023-04-28 12:33:10 +0100

    nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lGreen: "#25DAC5",
+        lHeading: "#1e0e62",
+        lGray: "#808080",
+        lBlue: "#2F1893",
+        lPush: "#E93A7D",
+        lred: "#EA3223",
+        offwhite: "#E4E3ED",
+      },
+      gridTemplateColumns: {
+        'custom': '57% 42%',
+      },
+    },
   },
   plugins: [require("daisyui")],
-}
+};
